f128f58794434825c8ba79c3453cd07fcd07775a
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-03 22:32:41 +0500

    This is a task no12

---
+++ index.ts
@@ -0,0 +1,12 @@
+//Tasdk # 12//
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s
+// name, print a message to them. The text of each message should be the same, but each message should
+//be personalized with the person’s name.//
+
+let names: string [] =["sana", "ayesha", "munza", "inza", "bisma"];
+let message: string = "Do you like to read some books?"
+console.log(names[0] + " "+ message)
+console.log(names[1]+ " " + message)
+console.log(names[2]+ " " + message)
+console.log(names[3]+ " " + message)
+console.log(names[4]+ " " + message) 
